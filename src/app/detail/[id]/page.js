dbc430edfc20eef6870a16f21ba45432fb536160
이명섭 <abbbc@mz.co.kr>
2023-05-08 18:41:51 +0900

    create board

---
+++ src/app/detail/[id]/page.js
@@ -0,0 +1,17 @@
+import { ObjectId } from "mongodb";
+import { connectDB } from "../../../../util/database";
+
+export default async function Detail(props) {
+    console.log(props)
+    const db = (await connectDB).db("fourm")
+    let result = await db.collection('post').findOne({_id : new ObjectId(props.params.id)});
+    console.log(result)
+    
+    return (
+        <div>
+            <h4>상세페이지임</h4>
+            <h4>{result.title}</h4>
+            <p>{result.content}</p>
+        </div>
+    )
+}
